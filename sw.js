6511f909d28ecd7ae3e03928b5f409a3678272e5
Gilnei Laurindo <107190484+gilneilaurindo@users.noreply.github.com>
2026-04-04 17:33:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = 'total-saude-v1';
+
+self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-    e.waitUntil(clients.claim());
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', (e) => {
-    // Permite que o app funcione e passe na validação PWA
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return new Response("Offline - Total Saúde");
+        })
+    );
 });
